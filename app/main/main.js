c892da7b1e3e446f67f796f82976f3bf516691d8
kn <arielcessario@gmail.com>
2015-08-12 17:55:15 -0300

    Initial commit

---
+++ app/main/main.js
@@ -0,0 +1,52 @@
+(function () {
+    'use strict';
+    var scripts = document.getElementsByTagName("script")
+    var currentScriptPath = scripts[scripts.length-1].src;
+    angular.module('uiglp.main', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/main', {
+                templateUrl: currentScriptPath.replace('.js', '.html'),
+                controller: 'MainController'
+            });
+        }])
+        .controller('MainController', MainController);
+
+
+    MainController.$inject = [];
+    function MainController() {
+
+        var vm = this;
+        vm.agendaMes = 'DICIEMBRE';
+
+        vm.selectImage = selectImage;
+        vm.alerta = alerta;
+        vm.empresas = [
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''},
+            {nombre:'', foto:''}
+        ];
+
+        function alerta(){
+            var r = confirm('hoa');
+            if(r){
+                alert(r);
+            }
+        }
+
+
+
+        function selectImage(id){
+            console.log(id);
+        }
+    }
+})();
