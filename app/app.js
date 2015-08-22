3db45cf9ef23ba817d755f48677969d583b063df
kn <arielcessario@gmail.com>
2015-08-22 19:04:15 -0300

    v.01

--- app/app.js
+++ app/app.js
@@ -2,9 +2,26 @@
 
 // Declare app level module which depends on views, and components
 angular.module('uiglp', [
-  'ngRoute',
+    'ngRoute',
     'uiglp.main'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/main'});
-}]);
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/main'});
+    }])
+    .controller('MainController', MainController);
+
+
+MainController.$inject = [];
+function MainController() {
+
+    var vm = this;
+    vm.links = [
+        {nombre: 'INICIO', path: ''},
+        {nombre: 'INSTITUCIONAL', path: ''},
+        {nombre: 'BUSQUEDA LABORAL', path: ''},
+        {nombre: 'AGENDA', path: ''},
+        {nombre: 'CONTACTO', path: ''},
+        {nombre: 'REVISTA', path: ''}
+    ];
+
+}
