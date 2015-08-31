28811949e5c0b0549c28844819960e14b80c51b9
arielcessario <arielcessario@gmail.com>
2015-08-31 18:59:36 -0300

    a

--- app/administracion/administracion.js
+++ app/administracion/administracion.js
@@ -1,115 +1,1 @@
-(function () {
-    'use strict';
-    var scripts = document.getElementsByTagName("script")
-    var currentScriptPath = scripts[scripts.length - 1].src;
-    angular.module('uiglp.administracion', ['ngRoute'])
-        .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/administracion', {
-                templateUrl: currentScriptPath.replace('.js', '.html'),
-                controller: 'AdministracionController',
-                data: {requiresLogin:true}
-            });
-        }])
-        .controller('AdministracionController', AdministracionController);
-
-
-    AdministracionController.$inject = ['LoginService', 'LoginState', 'store', '$location', 'OfertasLaboralesService'];
-    function AdministracionController(LoginService, LoginState, store, $location, OfertasLaboralesService) {
-
-        var vm = this;
-
-        vm.usuario = {
-            cliente_id: -1,
-            nombre: '',
-            apellido: '',
-            nro_doc: '',
-            telefono: '',
-            password: '',
-            direccion: '',
-            mail: ''
-        };
-
-        vm.oferta_laboral = {
-            oferta_laboral_id: -1,
-            titulo: '',
-            detalle: ''
-        };
-
-        vm.usuarios = [];
-        vm.ofertas_laborales = [];
-
-        vm.saveUsuario = saveUsuario;
-        vm.updateUsuario = updateUsuario;
-        vm.modificarUsuario = modificarUsuario;
-
-        vm.saveOfertaLaboral = saveOfertaLaboral;
-        vm.updateOfertaLaboral = updateOfertaLaboral;
-        vm.modificarOfertaLaboral = modificarOfertaLaboral;
-        vm.removeOfertaLaboral = removeOfertaLaboral;
-
-        vm.logout = logout;
-
-        function logout(){
-            LoginState = false;
-            store.remove('jwt');
-            $location.path('/ingreso');
-        }
-
-        LoginService.getClientes(function (data) {
-            console.log(data);
-            vm.usuarios = data;
-        });
-
-        OfertasLaboralesService.get(function (data) {
-            vm.ofertas_laborales = data;
-        });
-
-
-        function modificarUsuario(usuario) {
-            vm.usuario = angular.copy(usuario);
-            vm.usuario.rol_id = '' + vm.usuario.rol_id;
-        }
-
-        function saveUsuario() {
-            LoginService.create(vm.usuario, function (data) {
-                console.log(data);
-            });
-
-        }
-
-        function updateUsuario() {
-            LoginService.updateCliente(vm.usuario, function (data) {
-                console.log(data);
-            });
-
-        }
-
-        function modificarOfertaLaboral(usuario) {
-            vm.usuario = angular.copy(usuario);
-            vm.usuario.rol_id = '' + vm.usuario.rol_id;
-        }
-
-        function saveOfertaLaboral() {
-            LoginService.create(vm.usuario, function (data) {
-                console.log(data);
-            });
-
-        }
-
-        function updateOfertaLaboral() {
-            LoginService.updateCliente(vm.usuario, function (data) {
-                console.log(data);
-            });
-
-        }
-
-        function removeOfertaLaboral() {
-            LoginService.updateCliente(vm.usuario, function (data) {
-                console.log(data);
-            });
-
-        }
-
-
-    }
-})();
+(function () {    'use strict';    var scripts = document.getElementsByTagName("script")    var currentScriptPath = scripts[scripts.length - 1].src;    angular.module('uiglp.administracion', ['ngRoute'])        .config(['$routeProvider', function ($routeProvider) {            $routeProvider.when('/administracion', {                templateUrl: currentScriptPath.replace('.js', '.html'),                controller: 'AdministracionController',                data: {requiresLogin:true}            });        }])        .controller('AdministracionController', AdministracionController);    AdministracionController.$inject = ['LoginService', 'LoginState', 'store', '$location', 'OfertasLaboralesService', 'jwtHelper'];    function AdministracionController(LoginService, LoginState, store, $location, OfertasLaboralesService, jwtHelper) {        var vm = this;        vm.usuario = {            cliente_id: -1,            nombre: '',            apellido: '',            nro_doc: '',            telefono: '',            password: '',            direccion: '',            mail: ''        };        vm.oferta_laboral = {            oferta_laboral_id: -1,            titulo: '',            detalle: ''        };        vm.usuarios = [];        vm.ofertas_laborales = [];        vm.saveUsuario = saveUsuario;        vm.updateUsuario = updateUsuario;        vm.modificarUsuario = modificarUsuario;        vm.saveOfertaLaboral = saveOfertaLaboral;        vm.updateOfertaLaboral = updateOfertaLaboral;        vm.modificarOfertaLaboral = modificarOfertaLaboral;        vm.removeOfertaLaboral = removeOfertaLaboral;        vm.logout = logout;        vm.user = jwtHelper.decodeToken(store.get('jwt'));        function logout(){            LoginState = false;            store.remove('jwt');            $location.path('/ingreso');        }        LoginService.getClientes(function (data) {            vm.usuarios = data;        });        if(vm.user.data.rol == 1){            OfertasLaboralesService.get(function (data) {                vm.ofertas_laborales = data;            });        }else{            OfertasLaboralesService.getByCliente(vm.user.data.userId,function (data) {                vm.ofertas_laborales = data;            });        }        function modificarUsuario(usuario) {            vm.usuario = angular.copy(usuario);            vm.usuario.rol_id = '' + vm.usuario.rol_id;        }        function saveUsuario() {            LoginService.create(vm.usuario, function (data) {                console.log(data);            });        }        function updateUsuario() {            LoginService.updateCliente(vm.usuario, function (data) {                console.log(data);            });        }        function modificarOfertaLaboral(usuario) {            vm.usuario = angular.copy(usuario);            vm.usuario.rol_id = '' + vm.usuario.rol_id;        }        function saveOfertaLaboral() {            LoginService.create(vm.usuario, function (data) {                console.log(data);            });        }        function updateOfertaLaboral() {            LoginService.updateCliente(vm.usuario, function (data) {                console.log(data);            });        }        function removeOfertaLaboral() {            LoginService.updateCliente(vm.usuario, function (data) {                console.log(data);            });        }    }})();
--- app/ofertas_laborales/ofertas_laborales.php
+++ app/ofertas_laborales/ofertas_laborales.php
@@ -1,99 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * Cliente: kn
- * Date: 16/03/15
- * Time: 19:13
- */
-
-session_start();
-if (file_exists('../../../MyDBi.php')) {
-    require_once '../../../MyDBi.php';
-} else {
-    require_once '../MyDBi.php';
-}
-
-
-$data = file_get_contents("php://input");
-
-$decoded = json_decode($data);
-
-if ($decoded != null) {
-    if ($decoded->function == 'create') {
-        create($decoded->item);
-    } else if ($decoded->function == 'update') {
-        update($decoded->item);
-    } else if ($decoded->function == 'delete') {
-        delete($decoded->item);
-    }
-} else {
-    $function = $_GET["function"];
-    if ($function == 'get') {
-        get();
-    }
-}
-
-
-function get()
-{
-    $db = new MysqliDb();
-    $results = $db->get('ofertas_laborales');
-    echo json_encode($results);
-}
-
-function delete($item)
-{
-    $db = new MysqliDb();
-    $decoded = json_decode($item);
-
-    $db->where('oferta_laboral_id', $decoded->oferta_laboral_id);
-
-    $results = $db->delete('ofertas_laborales');
-    echo json_encode($results);
-}
-
-function create($item)
-{
-    $db = new MysqliDb();
-    $decoded = json_decode($item);
-    $data = array(
-        'titulo' => $decoded->titulo,
-        'detalle' => $decoded->detalle,
-        'cliente_id' => 1/*El cliente viene de JWT*/,
-        'status' => 1
-    );
-
-    $result = $db->insert('ofertas_laborales', $data);
-    if ($result > -1) {
-        echo json_encode(true);
-    } else {
-        echo json_encode(false);
-    }
-}
-
-/**
- * esta funcion me retorna un cliente filtrando x email
- * @param $email
- */
-
-function update($item)
-{
-    $db = new MysqliDb();
-    $decoded = json_decode($item);
-
-    $db->where('oferta_laboral_id', $decoded->oferta_laboral_id);
-
-
-    $data = array(
-        'titulo' => $decoded->titulo,
-        'detalle' => $decoded->detalle,
-        'cliente_id' => 1/*El cliente viene de JWT*/,
-        'status' => 1
-    );
-
-    if ($db->update('ofertas_laborales', $data)) {
-        echo json_encode(['result' => true]);
-    } else {
-        echo json_encode(['result' => false]);
-    }
-}
+<?php/** * Created by PhpStorm. * Cliente: kn * Date: 16/03/15 * Time: 19:13 */session_start();if (file_exists('../../../MyDBi.php')) {    require_once '../../../MyDBi.php';} else {    require_once '../MyDBi.php';}$data = file_get_contents("php://input");$decoded = json_decode($data);if ($decoded != null) {    if ($decoded->function == 'create') {        create($decoded->item);    } else if ($decoded->function == 'update') {        update($decoded->item);    } else if ($decoded->function == 'delete') {        delete($decoded->item);    }} else {    $function = $_GET["function"];    if ($function == 'get') {        get();    }}function get(){    $db = new MysqliDb();    $results = $db->get('select fecha, titulo, detalle, 0 cliente from ofertas_laborales');    foreach($results as $key => $row){        $db->where('cliente_id', $row["cliente_id"]);        $cliente = $db->get('clientes');        $results[$key]["cliente"] = $cliente;    }    echo json_encode($results);}function delete($item){    $db = new MysqliDb();    $decoded = json_decode($item);    $db->where('oferta_laboral_id', $decoded->oferta_laboral_id);    $results = $db->delete('ofertas_laborales');    echo json_encode($results);}function create($item){    $db = new MysqliDb();    $decoded = json_decode($item);    $data = array(        'titulo' => $decoded->titulo,        'detalle' => $decoded->detalle,        'cliente_id' => 1/*El cliente viene de JWT*/,        'status' => 1    );    $result = $db->insert('ofertas_laborales', $data);    if ($result > -1) {        echo json_encode(true);    } else {        echo json_encode(false);    }}/** * esta funcion me retorna un cliente filtrando x email * @param $email */function update($item){    $db = new MysqliDb();    $decoded = json_decode($item);    $db->where('oferta_laboral_id', $decoded->oferta_laboral_id);    $data = array(        'titulo' => $decoded->titulo,        'detalle' => $decoded->detalle,        'cliente_id' => 1/*El cliente viene de JWT*/,        'status' => 1    );    if ($db->update('ofertas_laborales', $data)) {        echo json_encode(['result' => true]);    } else {        echo json_encode(['result' => false]);    }}
