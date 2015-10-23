2cab69ad33c2aee0c8463c0dd43a4466c5b35b09
DiegoYankelevich <diegoyankelevich@gmail.com>
2015-10-23 02:03:04 -0300

    diego

--- app/ac-ripple-button/ac-ripple-button.js
+++ app/ac-ripple-button/ac-ripple-button.js
@@ -1,59 +1,59 @@
-(function () {
-    'use strict';
-
-    var scripts = document.getElementsByTagName("script");
-    var currentScriptPath = scripts[scripts.length - 1].src;
-
-    angular.module('acRipple', ['ngRoute'])
-        .directive('acRipple', AcRipple);
-
-
-    AcRipple.$inject = ['$location', '$timeout'];
-
-    function AcRipple($location, $timeout) {
-        return {
-            restrict: 'C',
-            scope: {
-
-                rippleColor: '@'
-            },
-            controller: AcRippleController,
-            link: function (scope, element, attrs) {
-                element.on('click', function (event) {
-                    event.preventDefault();
-
-                    var $div = angular.element('<div></div>'),
-                    //btnOffset = $(this).offset(),
-                        xPos = event.pageX - element[0].offsetLeft,
-                        yPos = event.pageY - element[0].offsetTop;
-
-                    $div.addClass('ripple-effect');
-                    //var $ripple = angular.element(".ripple-effect");
-                    var $ripple = $div;
-
-                    $ripple.css("height", element[0].clientHeight);
-                    $ripple.css("width", element[0].clientHeight);
-                    $div.css({
-                        top: yPos + 'px',
-                        left: xPos + 'px',
-                        background: scope.rippleColor
-                    });
-                    element.append($div);
-
-                    $timeout(function () {
-                        $div.remove();
-                    }, 2000);
-                });
-            },
-            controllerAs: 'AcRippleCtrl'
-        };
-    }
-
-
-    AcRippleController.$inject = ["$http", "$scope"];
-    function AcRippleController($http, $scope) {
-        var vm = this;
-
-    }
-
-})();
+(function () {
+    'use strict';
+
+    var scripts = document.getElementsByTagName("script");
+    var currentScriptPath = scripts[scripts.length - 1].src;
+
+    angular.module('acRipple', ['ngRoute'])
+        .directive('acRipple', AcRipple);
+
+
+    AcRipple.$inject = ['$location', '$timeout'];
+
+    function AcRipple($location, $timeout) {
+        return {
+            restrict: 'C',
+            scope: {
+
+                rippleColor: '@'
+            },
+            controller: AcRippleController,
+            link: function (scope, element, attrs) {
+                element.on('click', function (event) {
+                    event.preventDefault();
+
+                    var $div = angular.element('<div></div>'),
+                    //btnOffset = $(this).offset(),
+                        xPos = event.pageX - element[0].offsetLeft,
+                        yPos = event.pageY - element[0].offsetTop;
+
+                    $div.addClass('ripple-effect');
+                    //var $ripple = angular.element(".ripple-effect");
+                    var $ripple = $div;
+
+                    $ripple.css("height", element[0].clientHeight);
+                    $ripple.css("width", element[0].clientHeight);
+                    $div.css({
+                        top: yPos + 'px',
+                        left: xPos + 'px',
+                        background: scope.rippleColor
+                    });
+                    element.append($div);
+
+                    $timeout(function () {
+                        $div.remove();
+                    }, 2000);
+                });
+            },
+            controllerAs: 'AcRippleCtrl'
+        };
+    }
+
+
+    AcRippleController.$inject = ["$http", "$scope"];
+    function AcRippleController($http, $scope) {
+        var vm = this;
+
+    }
+
+})();
--- app/institucional/institucional.js
+++ app/institucional/institucional.js
@@ -1,29 +1,29 @@
-(function () {
-    'use strict';
-    var scripts = document.getElementsByTagName("script");
-    var currentScriptPath = scripts[scripts.length - 1].src;
-    angular.module('uiglp.institucional', ['ngRoute'])
-        .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/institucional', {
-                templateUrl: currentScriptPath.replace('.min.js', '.html'),
-                controller: 'InstitucionalController',
-                data: {requiresLogin:false}
-            });
-        }])
-        .controller('InstitucionalController', InstitucionalController);
-
-
-    InstitucionalController.$inject = ['LoginService', 'LoginState', 'store', '$location'];
-    function InstitucionalController(LoginService, LoginState, store, $location) {
-
-        var vm = this;
-
-        vm.seccion = 1;
-
-        function changeSeccion(){
-            institucionalCtrl.seccion = (institucionalCtrl.seccion == 1)? 1:institucionalCtrl.seccion + 1;
-        }
-
-
-    }
+(function () {
+    'use strict';
+    var scripts = document.getElementsByTagName("script");
+    var currentScriptPath = scripts[scripts.length - 1].src;
+    angular.module('uiglp.institucional', ['ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/institucional', {
+                templateUrl: currentScriptPath.replace('.min.js', '.html'),
+                controller: 'InstitucionalController',
+                data: {requiresLogin:false}
+            });
+        }])
+        .controller('InstitucionalController', InstitucionalController);
+
+
+    InstitucionalController.$inject = ['LoginService', 'LoginState', 'store', '$location'];
+    function InstitucionalController(LoginService, LoginState, store, $location) {
+
+        var vm = this;
+
+        vm.seccion = 1;
+
+        function changeSeccion(){
+            institucionalCtrl.seccion = (institucionalCtrl.seccion == 1)? 1:institucionalCtrl.seccion + 1;
+        }
+
+
+    }
 })();
