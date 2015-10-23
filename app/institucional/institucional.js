c33f38a7893ddc578a62d33f11af68c5c26dfdf4
arielcessario <arielcessario@gmail.com>
2015-10-23 11:42:39 -0300

    a

--- app/institucional/institucional.js
+++ app/institucional/institucional.js
@@ -7,7 +7,7 @@
             $routeProvider.when('/institucional', {
                 templateUrl: currentScriptPath.replace('.min.js', '.html'),
                 controller: 'InstitucionalController',
-                data: {requiresLogin:true}
+                data: {requiresLogin:false}
             });
         }])
         .controller('InstitucionalController', InstitucionalController);
@@ -19,10 +19,6 @@
         var vm = this;
 
         vm.seccion = 1;
-
-        function changeSeccion(){
-            institucionalCtrl.seccion = (institucionalCtrl.seccion == 1)? 1:institucionalCtrl.seccion + 1;
-        }
 
 
     }
